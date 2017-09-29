7af699b48399b5b4b3c32881019e3543b432e297
Juliana Capador <30811082+JulianaCapador@users.noreply.github.com>
2017-09-29 07:48:07 -0500

    Add files via upload

--- CatalogoPersonajes/Builder/ConstructorP.py
+++ CatalogoPersonajes/Builder/ConstructorP.py
@@ -6,4 +6,39 @@ Created on 20/09/2017
 import FabricaAbstracta.ProductosAbstractos
 import SimpleFactory.Personajes
 
-#Clase Director
+class Director:
+    def __init__(self):
+        self.builder = None
+        
+    def SetBuilder(self, builderObj):
+        self.builder = builderObj
+        
+    def Construct (self, name):
+        if name == "OrcoArmado":
+            self.builder.Create()
+            self.builder.BuildPartA()
+            self.builder.BuildPartB()
+        
+        elif name == "ElfoArmado":
+            self.builder.Create()
+            self.builder.BuildPartC()
+            self.builder.BuildPartD()
+            
+        elif name == "MagoArmado":
+            self.builder.Create()
+            self.builder.BuildPartC()
+            self.builder.BuildPartD()   
+            
+class Builder:
+    
+    def Create(self):
+        raise NotImplementedError("Create()must defined in subclass")
+    def BulidPArtA(self):
+        raise NotImplementedError("BuildPartA()must defined in subclass")
+    def BulidPArtB(self):
+        raise NotImplementedError("BuildPartB()must defined in subclass")
+    def BulidPArtC(self):
+        raise NotImplementedError("BuildPartC()must defined in subclass")
+    def BulidPArtD(self):
+        raise NotImplementedError("BuildPartD()must defined in subclass")
+     
